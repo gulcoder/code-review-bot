b8fb30c62b801732db0bd0e06da758d39469a1f3
Gul Baran <gulbaran243@gmail.com>
2025-08-07 13:48:23 +0200

    fixup! fixup! auto-refactor bad_code2.py

--- bad_code2.py
+++ bad_code2.py
@@ -1,21 +1,17 @@
 3a4
-> import subprocess
-6c7
-<     subprocess.call("ls -la", shell=True)
----
->     subprocess.run(["ls", "-la"], check=True)
+> import logging
 9,10d9
 < def duplicate_code():
 <     subprocess.call("ls -la", shell=True)
-13,17c12,15
+13,17c12,14
 < for i in range(0, 10):
 <     if i % 2 == 0:
 <         logging.info(i)
 <     else:
 <         logging.info(i)
 ---
+> logging.basicConfig(level=logging.INFO)
+> 
 > for i in range(10):
->     if i % 2 == 0:
->         logging.info(f"{i} is even")
->     else:
->         logging.info(f"{i} is odd")
+>     parity = "even" if i % 2 == 0 else "odd"
+>     logging.info(f"{i} is {parity}")
