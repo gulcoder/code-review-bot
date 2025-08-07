14e451237310dcc08775895023c4c50e1755ce36
Gül Baran <gulbaran243@gmail.com>
2025-08-07 11:05:47 +0200

    Update test_change.py

--- test_change.py
+++ test_change.py
@@ -1,2 +1,11 @@
 # test_change.py
+
+import subprocess
+import os
+
+def bad_function():
+    subprocess.call("ls -l", shell=True)  # bör ändras till lista och utan shell=True
+
+bad_function()
+
 print("Detta är en teständring som boten borde refaktorera.")
