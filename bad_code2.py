367a1b2fddabd142252624ac6c99c357b21bea9a
Gul Baran <gulbaran243@gmail.com>
2025-08-07 12:02:17 +0200

    Förbättrad kod: säkrare subprocess-anrop och kodrensning

--- bad_code2.py
+++ bad_code2.py
@@ -1,15 +1,22 @@
-# bad_code2.py
-
-import subprocess
-
-def list_files():
-    subprocess.call("ls -la", shell=True)
-
-def duplicate_code():
-    subprocess.call("ls -la", shell=True)
-
-for i in range(0, 10):
-    if i % 2 == 0:
-        logging.info(i)
-    else:
-        logging.info(i)
+1c1
+< # bad_code2.py
+---
+> # improved_code.py
+3a4
+> import logging
+6c7
+<     subprocess.call("ls -la", shell=True)
+---
+>     subprocess.call(["ls", "-la"])
+9,10d9
+< def duplicate_code():
+<     subprocess.call("ls -la", shell=True)
+13,17c12,13
+< for i in range(0, 10):
+<     if i % 2 == 0:
+<         logging.info(i)
+<     else:
+<         logging.info(i)
+---
+> # Korrekt loggning av i
+> for i in range(0, 10):
